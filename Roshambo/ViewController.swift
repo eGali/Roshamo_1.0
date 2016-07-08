9b5a5e2b36b89ce639f06db6877a9ba603869ba5
Edgar <EGalindo4@gmail.com>
2016-07-08 14:40:21 -0700

    Initial Commit

---
+++ Roshambo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roshambo
+//
+//  Created by Edgar on 7/8/16.
+//  Copyright © 2016 Edgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
